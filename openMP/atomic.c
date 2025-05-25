7b1644080f9cb95c7d5f96ed15c98a6e3360df03
H.L.D.U.G Perera <138327662+UmeshGayashan@users.noreply.github.com>
2025-05-25 23:03:36 +0530

    less resource usage than critical section

---
+++ openMP/atomic.c
@@ -0,0 +1,36 @@
+// Thread Synchronization
+#include <stdio.h>
+#include <omp.h>
+
+float f(int x) {
+    return x * 1.0f;  // convert int to float
+}
+
+int main(){
+    float result = 0.0;
+    
+    #pragma omp parallel
+    {
+        float B;
+        int i, id, nthreads;
+
+        id = omp_get_thread_num();
+        nthreads = omp_get_num_threads();
+        float local_result = 0.0;
+
+        for (i = id; i < nthreads; i++) {
+            B = f(i);
+            local_result += B;
+        }
+        // Atomic Operation - Mutual exclusion only applies to update of a memory location
+        // x binop = expr
+        // x++
+        // ++x
+        // x--
+        // --x
+            #pragma omp atomic
+            result += local_result;
+        }
+    printf("Final result: %f\n", result);
+    return 0;
+}
